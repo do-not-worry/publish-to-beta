7757115a8fb0066a5ecf4023cd0592b265efd609
shux <shux@mingyuanyun.com>
2019-03-30 17:41:50 +0800

    准备合并

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # publish-to-beta
 # 发布代码至beta环境，避免大量合并分支、打tag及构建操作
+# 将该文件夹放在代码根目录下clone
 # 最终实现效果：window的bat脚本 => git bash 命令行 => 触发shell脚本，方便的一批
--- publish.sh
+++ publish.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 #发布代码至beta环境,避免大量合并、tag及构建操作
 
+cd ../
 
-
-
-
-
-
-
-if [ -n "$1" ];then
-    echo 'YOUZHI'
-else
-    echo 'NONONO'
+currentBranch=`git branch | grep '*' | sed "s/^* //g"`
+if [ ${currentBranch} != 'master' ];then
+    echo "请先切换至master分支！"
+    exit
 fi
+git pull
+
+
+
+
+#if [ -n "$1" ];then
+#    echo 'YOUZHI'
+#else
+#    echo 'NONONO'
+#fi
