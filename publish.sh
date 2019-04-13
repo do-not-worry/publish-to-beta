05710d1f45d89518ff54b14e105e06a7fa725921
shux <shux@mingyuanyun.com>
2019-04-13 12:11:57 +0800

    准备发布测试

--- publish.sh
+++ publish.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-#发布代码至beta环境,避免大量合并、tag及构建操作
+#自动发布代码至beta环境,避免大量合并、tag及构建操作
+#$1要合并的分支名,无则不传
 
 cd ../
 
@@ -8,13 +9,63 @@ if [ ${currentBranch} != 'master' ];then
     echo "请先切换至master分支！"
     exit
 fi
+
 git pull
 
+#如果传了分支名,则合并分支并push,否则直接打tag
+if [ -n "$1" ];then
+    mergeBranch="origin/"$1
+    git merge ${mergeBranch}
+    git push origin master
+fi
+
+#打tag
+tag="M"`date +"%Y%m%d%H%M%S"`
+git tag ${tag}
+git push origin ${tag}
 
 
 
-#if [ -n "$1" ];then
-#    echo 'YOUZHI'
-#else
-#    echo 'NONONO'
-#fi
+#准备构建beta环境
+CONFIG_FILE=./publish.conf
+COOKIE_FILE=./cookie.txt
+userAgent="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"
+retryNum=0
+
+#构建
+function build()
+{
+    buildUrl=`cat ${CONFIG_FILE} | grep "buildUrl" | awk -F"buildUrl=" '{print($2)}'`
+    crumb=`cat ${CONFIG_FILE} | grep "Jenkins-Crumb" | awk -F"Jenkins-Crumb=" '{print($2)}'`
+
+    #urlencode
+    jsonStr='{"parameter": {"name": "tag", "value": "'${tag}'"}, "statusCode": "303", "redirectTo": ".", "Jenkins-Crumb": "'${crumb}'"}'
+    jsonStr=`echo ${jsonStr} | tr -d '\n' | od -An -tx1 | tr ' ' %`
+    buildData="name=tag&value=${tag}&statusCode=303&redirectTo=.&Jenkins-Crumb=${crumb}&json=${jsonStr}&Submit=%E5%BC%80%E5%A7%8B%E6%9E%84%E5%BB%BA"
+    buildRes=`curl -d "${buildData}" -A "${userAgent}" -b "${COOKIE_FILE}" "${buildUrl}"`
+    if [[ ${buildRes} =~ "login" ]];then
+        echo "登录信息已过期,重新登录中..."
+        doLogin
+
+        if [ ${retryNum} -gt 3 ];then
+            echo "重新登录超过3次,已自动退出！"
+            exit
+        fi
+
+        build
+    else
+        echo "发布成功！tag为："${tag}
+    fi
+}
+
+#登录
+function doLogin()
+{
+    let retryNum=retryNum+1
+	loginUrl=`cat ${CONFIG_FILE} | grep 'loginUrl' | awk -F'loginUrl=' '{print($2)}'`
+	username=`cat ${CONFIG_FILE} | grep 'username' | awk -F'username=' '{print($2)}'`
+	password=`cat ${CONFIG_FILE} | grep 'password' | awk -F'password=' '{print($2)}'`
+	postData="j_username=${username}&j_password=${password}&from=/&Submit=登录"
+
+	curl -d ${postData} -A "${userAgent}" -c "${COOKIE_FILE}" "${loginUrl}"
+}
