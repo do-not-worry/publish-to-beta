e1d078c980c3dc34c114dd613c8fd8abb92b47d3
shux <shux@mingyuanyun.com>
2019-04-13 16:05:20 +0800

    可以发布至beta环境

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 # 正式环境可直接用生成的这个tag构建
 # 将该项目放在代码根目录下clone作为子模块
 # 为了方便运行,桌面快捷方式 => beta.bat生成git-bash窗口
-# git-bash -c、--command无法调用linux指令
+# git-bash -c、--command无法调用linux指令,故暂时无法直接调用
--- publish.sh
+++ publish.sh
@@ -26,7 +26,7 @@ git push origin ${tag}
 
 
 
-#准备构建beta环境
+#######################################准备构建beta环境
 CONFIG_FILE=./publish.conf
 COOKIE_FILE=./cookie.txt
 userAgent="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"
@@ -35,19 +35,16 @@ retryNum=0
 #构建
 function build()
 {
-    buildUrl=`cat ${CONFIG_FILE} | grep "buildUrl" | awk -F"buildUrl=" '{print($2)}'`
-    crumb=`cat ${CONFIG_FILE} | grep "Jenkins-Crumb" | awk -F"Jenkins-Crumb=" '{print($2)}'`
-
-    #urlencode
-    jsonStr='{"parameter": {"name": "tag", "value": "'${tag}'"}, "statusCode": "303", "redirectTo": ".", "Jenkins-Crumb": "'${crumb}'"}'
-    jsonStr=`echo ${jsonStr} | tr -d '\n' | od -An -tx1 | tr ' ' %`
-    buildData="name=tag&value=${tag}&statusCode=303&redirectTo=.&Jenkins-Crumb=${crumb}&json=${jsonStr}&Submit=%E5%BC%80%E5%A7%8B%E6%9E%84%E5%BB%BA"
+    buildUrl=`cat ${CONFIG_FILE} | grep 'buildUrl' | awk -F'buildUrl=' '{print($2)}'`
+    crumb=`cat ${CONFIG_FILE} | grep 'Jenkins-Crumb' | awk -F'Jenkins-Crumb=' '{print($2)}'`
+    buildData="name=tag&value=${tag}&statusCode=303&redirectTo=.&Jenkins-Crumb=${crumb}&json=%7B%22parameter%22%3A+%7B%22name%22%3A+%22tag%22%2C+%22value%22%3A+%22${tag}%22%7D%2C+%22statusCode%22%3A+%22303%22%2C+%22redirectTo%22%3A+%22.%22%2C+%22Jenkins-Crumb%22%3A+%22${crumb}%22%7D&Submit=%E5%BC%80%E5%A7%8B%E6%9E%84%E5%BB%BA"
     buildRes=`curl -d "${buildData}" -A "${userAgent}" -b "${COOKIE_FILE}" "${buildUrl}"`
-    if [[ ${buildRes} =~ "login" ]];then
+
+    if [[ ${buildRes} =~ "403" ]];then
         echo "登录信息已过期,重新登录中..."
         doLogin
 
-        if [ ${retryNum} -gt 3 ];then
+        if [[ ${retryNum} -eq 3 ]];then
             echo "重新登录超过3次,已自动退出！"
             exit
         fi
@@ -65,7 +62,9 @@ function doLogin()
 	loginUrl=`cat ${CONFIG_FILE} | grep 'loginUrl' | awk -F'loginUrl=' '{print($2)}'`
 	username=`cat ${CONFIG_FILE} | grep 'username' | awk -F'username=' '{print($2)}'`
 	password=`cat ${CONFIG_FILE} | grep 'password' | awk -F'password=' '{print($2)}'`
-	postData="j_username=${username}&j_password=${password}&from=/&Submit=登录"
+	postData="j_username=${username}&j_password=${password}&from=%2F&Submit=%E7%99%BB%E5%BD%95"
 
 	curl -d ${postData} -A "${userAgent}" -c "${COOKIE_FILE}" "${loginUrl}"
 }
+
+build
